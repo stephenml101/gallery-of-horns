9551b85e05839d76965703da249cb878b3f6f8a7
Stephen <stephenlevesque2@gmail.com>
2023-03-16 15:17:49 -0700

    lab 4

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import './Hornedbeast.css'
 
 class Hornedbeast extends React.Component {
   constructor(props){
